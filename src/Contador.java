9c9ea0dd22b0feee4d5e0603d318c008f2650554
bootcamp2024 <bruno.bm12@gmail.com>
2024-05-21 16:08:16 -0300

    Arquivos atualizados

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Contador {
+import java.util.Scanner;
+
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        int parametroUm, parametroDois;
+
+        System.out.println("Digite o primeiro parâmetro");
+        parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException e){
+
+            System.out.println(e.getMessage());
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+
+        int contagem = parametroDois - parametroUm;
+
+        if(parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior do que o primeiro!");
+        }
+        else {
+
+            for (int i = 1; i < contagem; i++) {
+                System.out.println("Imprimindo o número: " + i);
+            }
+
+            System.out.println("\nTotal de interações: " + contagem);
+        }
+    }
 }
--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
 }
